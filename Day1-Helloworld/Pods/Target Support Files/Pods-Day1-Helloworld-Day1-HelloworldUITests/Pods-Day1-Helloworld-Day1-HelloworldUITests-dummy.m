7d6b8a9a6db91f1a74489c99aa14dffe08ce0631
le-xuan-quynh <le-xuan-quynh@goodcycle.net>
2019-12-04 14:10:40 +0900

    add pod

---
+++ Day1-Helloworld/Pods/Target Support Files/Pods-Day1-Helloworld-Day1-HelloworldUITests/Pods-Day1-Helloworld-Day1-HelloworldUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Day1_Helloworld_Day1_HelloworldUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Day1_Helloworld_Day1_HelloworldUITests
+@end
