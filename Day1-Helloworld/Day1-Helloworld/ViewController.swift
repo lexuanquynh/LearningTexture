7a3bd9367e952045292a30c5adf07670fff79d90
le-xuan-quynh <le-xuan-quynh@goodcycle.net>
2019-12-04 13:41:11 +0900

    init project

---
+++ Day1-Helloworld/Day1-Helloworld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day1-Helloworld
+//
+//  Created by Le Xuan Quynh on 2019/12/04.
+//  Copyright © 2019 Le Xuan Quynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
